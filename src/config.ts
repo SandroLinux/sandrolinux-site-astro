b003558c9023c07972e6dc7af448cb0335fef8e8
Sandro Linux <sandrolinux@outlook.com>
2023-01-22 19:15:26 +0000

    first commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sandro Linux';
+export const SITE_DESCRIPTION = 'Welcome to my site!';
+export const SITE_TITLE_HEAD = 'Sandro Linux';
+export const NAME = 'Oscar Sanderson';
+export const COPYNOTICE = 'All rights reserved';
