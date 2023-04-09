a5b6e86b15086129f2c09e638cbc215719fc861b
SandroLinux <sandrolinux@outlook.com>
2023-04-09 20:52:54 +0100

    🔍 Adds robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sandrolinux.com',
-	lastmod: new Date('2023-01-24'),
-	integrations: [mdx(), sitemap()],
+  site: 'https://sandrolinux.com',
+  lastmod: new Date('2023-01-24'),
+  integrations: [mdx(), sitemap(), robotsTxt({policy: [{userAgent: '*', allow: '/'}]  })]
 });
